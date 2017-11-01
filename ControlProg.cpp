fda4e13c27691d8001106c8c806374c3ed836674
eric <eolsen4@binghamton.edu>
2017-10-31 21:27:14 -0400

    skeleton created for control program

---
+++ ControlProg.cpp
@@ -0,0 +1,32 @@
+#include <stdin>
+#include <string>
+#include <map>
+using namespace std;
+
+
+int main()
+{
+ /* TODO Read in port/address info for each Node in network */
+ 
+ while(1)
+ {
+  String input;
+  int node1, node2;
+  
+  cout << "Enter Command: ";
+  cin >> input >> node1 >> node2;
+
+  if(input.equals("generate-packet"))
+  {
+
+  }
+  else if(input.equals("create-link"))
+  {
+
+  }
+  else
+  {
+
+  }
+ }
+}
